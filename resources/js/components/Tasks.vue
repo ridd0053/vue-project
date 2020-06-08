7918777ab6e1a1d46a1ef5210cdb36e978520352
ridd0053 <ridd0053@hz.nl>
2020-06-08 21:32:34 +0200

    status and comleted don't show

--- resources/js/components/Tasks.vue
+++ resources/js/components/Tasks.vue
@@ -194,7 +194,7 @@ export default {
                 console.log(response);
                 console.log(this.completed);
                 
-                location.reload();
+                // location.reload();
             })
             
             
